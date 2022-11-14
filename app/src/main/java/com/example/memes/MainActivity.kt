ea660d1a59ef44a5784454bdfebcb9ef7326bb72
Manik <manbhatia007@gmail.com>
2022-11-15 00:34:30 +0530

    data done

--- app/src/main/java/com/example/memes/MainActivity.kt
+++ app/src/main/java/com/example/memes/MainActivity.kt
@@ -3,19 +3,46 @@ package com.example.memes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        loadMeme()
     }
 
-    fun NextMeme(view: View) {
+    private fun loadMeme() {
+        val queue = Volley.newRequestQueue(this)
+        val url ="https://meme-api.herokuapp.com/gimme"
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            Response.Listener { response ->
+                val url=response.getString("url")
+                Glide.with(this).load(url).into(memeimageView)
+
+
+            },
+            Response.ErrorListener { Toast.makeText(this, "ERROR ", Toast.LENGTH_LONG).show() })
+
+        queue.add(jsonRequest)
 
 
     }
+
+    fun nextMeme(view: View) {
+
+
+    }
+
     fun shareMeme(view: View) {
-
 
 
     }
